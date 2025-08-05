1a19170e3bed6ddea1dbdeac4221821a753777a3
Aquarat <aquarat.t@ku.th>
2025-08-05 16:01:03 +0700

    fix validate line kid_edit_profile

--- pages/kids/kid_edit_profile/[userId]/[id].vue
+++ pages/kids/kid_edit_profile/[userId]/[id].vue
@@ -107,7 +107,6 @@ const updateKidProfile = async () => {
                         <div class="flex gap-4">
                             <input v-model="form.beaconId" type="text" placeholder="beacon id"
                                 class="block w-full rounded-md border border-gray-300 px-3 py-2 text-gray-900 shadow-sm focus:border-[#0198FF] focus:ring-[#0198FF]" />
-                            <p v-if="errors.beaconId" class="text-red-500 text-sm mt-1">{{ errors.beaconId }}</p>
 
                             <NuxtLink to="/kids/qrcode">
                                 <button type="submit">
@@ -115,7 +114,11 @@ const updateKidProfile = async () => {
                                         class="w-14 h-12 p-2 -mb-1 border-2 border-[#0198FF] rounded-md">
                                 </button>
                             </NuxtLink>
+
+
                         </div>
+                        <p v-if="errors.beaconId" class="text-red-500 text-sm mt-1">{{ errors.beaconId }}</p>
+
                     </div>
 
                     <div>
