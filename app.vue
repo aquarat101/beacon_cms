879464e536e62374f6323d77eba1003708758b4b
NM-ATX <tharchakon.s@kisracorp.com>
2025-08-08 16:19:48 +0700

    add liff id to env

--- app.vue
+++ app.vue
@@ -1,18 +1,15 @@
 <script>
 import liff from '@line/liff'
 
-liff.init({ liffId: '2007896109-67ZKrZlO' }).then(async () => {
-    try {
-        console.log(1)
+const { public: config } = useRuntimeConfig()
 
+liff.init({ liffId: config.liffId }).then(async () => {
+    try {
         if (!liff.isLoggedIn()) {
-            console.log(2)
             liff.login()
-            console.log(3)
         } else {
           console.log("Already logged in !!!")
         }
-        
     } catch (err) {
         console.error('LIFF init or login error:', err)
     }
