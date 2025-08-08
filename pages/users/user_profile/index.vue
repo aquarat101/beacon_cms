8d8a04808e2369b959c67febedbee066e60a4919
NM-ATX <tharchakon.s@kisracorp.com>
2025-08-08 19:02:43 +0700

    fix bugs

--- pages/users/user_profile/index.vue
+++ pages/users/user_profile/index.vue
@@ -62,26 +62,28 @@ try {
 
 <template>
   <div class="min-h-screen bg-white flex flex-col items-center text-[#035CB2]">
-
-    <!-- โหลดดิ้งโปรไฟล์ -->
-    <div v-if="loadingProfile" class="flex justify-center items-center h-64 w-full">
-      <p class="text-gray-500">Loading profile...</p>
-    </div>
-    <div v-else class="relative w-full h-64">
+    <!-- กล่องรวม: ต้อง relative -->
+    <div class="relative w-full h-64">
+      <!-- รูป background อยู่ข้างล่าง -->
       <img src="/images/background.png" alt="Register Header"
            class="absolute inset-0 w-full h-full object-cover z-0"/>
+
+      <!-- กล่องเนื้อหาซ้อนทับ -->
       <div class="absolute inset-0 flex flex-col items-start justify-center pl-10 z-10 gap-5">
         <div class="flex justify-between w-full">
           <h1 class="text-3xl font-bold text-outline-blue">Your Profile</h1>
+
           <NuxtLink :to="`/users/user_edit_profile/${userId}`">
             <button class="mr-8 mt-1 bg-[#035CB2] text-sm text-black rounded-full p-2 shadow z-10">
               <img src="/image-icons/edit.png" alt="edit" class="w-5 h-5">
             </button>
           </NuxtLink>
         </div>
+
         <div class="flex flex-row gap-5">
           <img :src="`${profile?.avatarUrl}`" alt="user profile" class="w-20 h-20 bg-white rounded-full">
-          <div>
+
+          <div class="">
             <p class="font-bold text-lg">{{ profile?.firstName }} {{ profile?.lastName }}</p>
             <p class="text-sm">{{ profile?.email }}</p>
             <p class="text-sm">{{ profile?.phone }}</p>
@@ -90,30 +92,31 @@ try {
       </div>
     </div>
 
+
     <!-- กล่องล่าง -->
     <div class="-mt-7 rounded-t-3xl bg-white px-8 py-6 w-full relative z-10">
       <div class="flex items-center justify-between mb-4">
         <h2 class="text-2xl font-bold text-blue-800">All Kids</h2>
         <NuxtLink :to="`/kids/kid_create_profile/${userId}`">
-          <button class="bg-[#0198FF] text-white rounded-full w-8 h-8 flex items-center justify-center">
-            <img src="/image-icons/plus.png" alt="create kid">
+          <button
+              class="bg-[#035CB2] text-white rounded-full w-8 h-8 text-4xl flex items-center justify-center">
+            <img src="/image-icons/plus.png" alt="create kid" class="w-4 h-4">
           </button>
         </NuxtLink>
       </div>
 
-      <!-- โหลดดิ้ง kids -->
-      <div v-if="loadingKids" class="text-center text-gray-500 py-5">
-        Loading kids...
-      </div>
-      <div v-else class="max-h-154 overflow-y-auto space-y-3 space-x-1.5">
+      <!-- ทำแนวตั้งด้วย flex + scroll -->
+      <div class="max-h-154 overflow-y-auto space-y-3 space-x-1.5">
         <template v-if="kids.length">
           <KidCard v-for="kid in kids" :key="kid.id" :userId="userId" :id="kid.id" :name="kid.name"
                    :status="kid.status" :updated="kid.updated" :avatarUrl="kid.avatarUrl"
                    class="min-w-[200px] shrink-0"/>
         </template>
         <p v-else class="mt-2 text-gray-500 text-center">No kids data</p>
+
       </div>
     </div>
+
   </div>
 </template>
 
