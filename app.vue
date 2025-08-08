e93282d5f219fba8cabf83c45877015a835ec888
NM-ATX <tharchakon.s@kisracorp.com>
2025-08-08 17:03:09 +0700

    render after initial

--- app.vue
+++ app.vue
@@ -1,13 +1,15 @@
-<script>
+<script setup>
 import liff from '@line/liff'
 
 const { public: config } = useRuntimeConfig()
+const loaded = ref(false)
 
 liff.init({ liffId: config.liffId }).then(async () => {
     try {
         if (!liff.isLoggedIn()) {
             liff.login()
         } else {
+          loaded.value = true
           console.log("Already logged in !!!")
         }
     } catch (err) {
@@ -17,7 +19,7 @@ liff.init({ liffId: config.liffId }).then(async () => {
 </script>
 
 <template>
-  <div>
+  <div v-if="loaded">
     <NuxtLayout>
       <NuxtPage/>
     </NuxtLayout>
