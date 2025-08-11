01715bdf02cd08c4c19ed383c05ed063d177d582
patipol.k <patipol.k@kisracorp.com>
2025-08-11 18:23:45 +0700

    [Fix] Responsive

--- pages/places/map/placeId/[userId]/[placeId].vue
+++ pages/places/map/placeId/[userId]/[placeId].vue
@@ -40,6 +40,8 @@ const isClearing = ref(false)
 
 const selectedPosition = ref(null)
 
+const isInputFocused = ref(false)
+
 // ปรับ onMapClick เรียกฟังก์ชันนี้แทน
 function onMapClick(event) {
     const latLng = event.latLng
@@ -422,6 +424,7 @@ watch(searchQuery, (val) => {
             <div class="mt-3 mb-4 mx-4 relative">
                 <input v-model="searchQuery" @keydown.enter.prevent="handleEnterKey" type="text"
                     placeholder="Search location"
+                    @focus="isInputFocused = true" @blur="isInputFocused = false"
                     class="w-full rounded-full px-4 pl-10 py-2 text-xl shadow-sm bg-white border border-white placeholder-gray-400" />
                 <span class="absolute left-4 top-3.5 text-gray-400">
                     <img src="/image-icons/search.png" alt="search" class="w-4 h-5" />
@@ -470,8 +473,8 @@ watch(searchQuery, (val) => {
         </div>
 
         <!-- Pin Result Place Section (ซ่อนเมื่อค้นหา) -->
-        <div v-if="showPlace && !showResults"
-            class="absolute bottom-0 w-full bg-white rounded-t-3xl text-lg p-6 shadow-lg">
+        <div v-if="!showResults && !showPlace && !isInputFocused"
+            class="fixed bottom-0 left-0 w-full bg-white text-xl rounded-t-3xl p-6 shadow-lg">
             <div class="flex items-start justify-between gap-4">
                 <div class="flex-1 min-w-0">
                     <p class="font-bold text-3xl text-[#035CB2] break-words">
